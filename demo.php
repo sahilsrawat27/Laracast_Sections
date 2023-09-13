152e4679824259f522245bdd2e408473a03c98d7
SahilSRawat <simransahil2113@gmail.com>
2023-09-12 23:28:28 -0700

    Create demo php file
    Add simple php code

---
+++ demo.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Demo</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            echo "Hello, Courtenay";
+            />
+        </h1>
+    </body>
+</html>
